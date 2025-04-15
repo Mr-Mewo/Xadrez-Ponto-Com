654c5f7c22be9e99d4bfdbc6414297a79dbe9cdf
Mr Mewo <0kuro.nekko0@gmail.com>
2025-04-15 22:17:47 +0100

    Making images work

---
+++ build-web.bat
@@ -0,0 +1,4 @@
+@echo off
+cd frontend
+trunk build --release
+pause
--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -4,12 +4,48 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
+            <NineSlice src="assets/img/tile-window.png" size=36/>
             <div class="board"></div>
-
+            <p class="title">{"Yew Game"}</p>
             <div class="side-panel">
                 <div class="panel-1"></div>
                 <div class="panel-2"></div>
             </div>
         </main>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct NineSliceProps {
+    pub src: String,
+    #[prop_or(36)]
+    pub size: u32,
+}
+
+#[function_component(NineSlice)]
+pub fn nine_slice(props: &NineSliceProps) -> Html {
+    let src = props.src.clone();
+    let size = props.size.clone();
+
+    let style = format!(
+        "--img: url('{}'); --size: {}px;",
+        props.src, props.size/3
+    );
+
+    html! {
+        <div class="nine-slice" style={style}>
+            <p>
+                // {props.src.clone()}
+            </p>
+            <div class="top-left"></div>
+            <div class="top"></div>
+            <div class="top-right"></div>
+            <div class="left"></div>
+            <div class="center"></div>
+            <div class="right"></div>
+            <div class="bottom-left"></div>
+            <div class="bottom"></div>
+            <div class="bottom-right"></div>
+        </div>
     }
 }
